bad1a00da4465bb497edb933cb595309626c28ac
Eke Péter <peterekepeter@gmail.com>
2019-12-28 17:51:20 +0200

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        modules: ["src", "node_modules"],
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    }
+};
