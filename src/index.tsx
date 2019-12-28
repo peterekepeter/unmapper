f9a1869b6848c82078287fb890cded56abf02022
Eke Péter <peterekepeter@gmail.com>
2019-12-29 00:50:34 +0200

    Github CI

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { Hello } from "./ui/Hello";
 
 ReactDOM.render(<Hello compiler="TypeScript" framework="React" />,document.body);
