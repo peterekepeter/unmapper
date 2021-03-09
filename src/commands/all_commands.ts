ffbb76e55a5808172eda4b6b62beb8af78266e5b
Eke Péter <peterekepeter@gmail.com>
2021-03-09 23:34:45 +0200

    Apply transform command

--- src/commands/all_commands.ts
+++ src/commands/all_commands.ts
@@ -1,18 +1,19 @@
 
-import { ICommandInfoV2 } from "../controller/command";
-import { create_polygon_command } from './create_polygon';
-import { extrude_polygons_command } from './extrude_polygons';
-import { delete_selected_command } from './delete_selected';
-import { toggle_vertex_mode_command } from './editor/toggle_vertex_mode';
-import { select_all_command } from './selection/select_all';
-import { align_vertexes_to_16_grid, align_vertexes_to_32_grid, align_vertexes_to_8_grid } from './align_vertexes_to_grid';
-import { shuffle_brush_polygons_command } from './shuffle_brush_polygons';
-import { triangulate_mesh_polygons_command } from './triangulate_mesh_polygons';
-import { uv_triplanar_map_command } from './uv_triplanar_map';
-import { flip_polygon_normal_command } from './flip_polygon_normal';
-import { toggle_editor_layout_command } from "./editor/toggle_editor_layout";
-import { move_command } from "./move";
-import { rotate_command } from "./rotate";
+import { ICommandInfoV2 } from "../controller/command"
+import { create_polygon_command } from './create_polygon'
+import { extrude_polygons_command } from './extrude_polygons'
+import { delete_selected_command } from './delete_selected'
+import { toggle_vertex_mode_command } from './editor/toggle_vertex_mode'
+import { select_all_command } from './selection/select_all'
+import { align_vertexes_to_16_grid, align_vertexes_to_32_grid, align_vertexes_to_8_grid } from './align_vertexes_to_grid'
+import { shuffle_brush_polygons_command } from './shuffle_brush_polygons'
+import { triangulate_mesh_polygons_command } from './triangulate_mesh_polygons'
+import { uv_triplanar_map_command } from './uv_triplanar_map'
+import { flip_polygon_normal_command } from './flip_polygon_normal'
+import { toggle_editor_layout_command } from "./editor/toggle_editor_layout"
+import { move_command } from "./move"
+import { rotate_command } from "./rotate"
+import { apply_transform_command } from "./apply_transform"
 
 
 export const get_all_commands_v2 : () => ICommandInfoV2[] = () => [
@@ -30,5 +31,6 @@ export const get_all_commands_v2 : () => ICommandInfoV2[] = () => [
     uv_triplanar_map_command,
     toggle_editor_layout_command,
     move_command,
-    rotate_command
+    rotate_command,
+    apply_transform_command
 ]
---
+++ src/commands/apply_transform.ts
@@ -0,0 +1,36 @@
+import { ICommandInfoV2 } from "../controller/command";
+import { change_selected_actors } from "../model/algorithms/editor_state_change";
+import { BrushVertex } from "../model/BrushVertex";
+import { EditorError } from "../model/EditorError";
+import { get_actor_to_world_transform } from "../model/geometry/actor-space-transform";
+import { Rotation } from "../model/Rotation";
+import { Scale } from "../model/Scale";
+import { Vector } from "../model/Vector";
+
+export const apply_transform_command: ICommandInfoV2 = {
+
+    description: "Apply object transformation to brush vertexes",
+
+    exec: state => change_selected_actors(state, old_actor => {
+        const transform_fn = get_actor_to_world_transform(old_actor)
+        if (state.vertex_mode) { throw new EditorError('does not work in vertex mode') }
+        if (transform_fn(Vector.ZERO) === Vector.ZERO &&
+            transform_fn(Vector.ONES) === Vector.ONES) {
+            return old_actor // identity transform detected
+        }
+        const old_brush = old_actor.brushModel
+        const new_brush = old_brush.shallow_copy()
+        new_brush.vertexes = old_brush.vertexes.map(v =>
+            new BrushVertex(transform_fn(v.position), v.selected))
+        const new_actor = old_actor.shallow_copy()
+        new_actor.rotation = Rotation.IDENTITY
+        new_actor.mainScale = Scale.DEFAULT_SCALE
+        new_actor.postScale = Scale.DEFAULT_SCALE
+        new_actor.location = Vector.ZERO
+        new_actor.oldLocation = null
+        new_actor.prePivot = null
+        new_actor.brushModel = new_brush
+        return new_actor
+    })
+
+}
