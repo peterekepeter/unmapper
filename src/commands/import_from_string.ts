8465f32b474ecf69d89bb7d42192edd2157452c8
Eke Péter <peterekepeter@gmail.com>
2021-08-01 22:00:52 +0300

    Imported actors are automatically selected

--- src/commands/import_from_string.ts
+++ src/commands/import_from_string.ts
@@ -1,12 +1,28 @@
-import { ICommandInfoV2 } from "../controller/command";
-import { change_actors_list } from "../model/algorithms/editor_state_change";
-import { EditorState } from "../model/EditorState";
-import { load_map_from_string } from "../model/loader";
+import { ICommandInfoV2 } from "../controller/command"
+import { EditorState } from "../model/EditorState"
+import { load_map_from_string } from "../model/loader"
 
 export const import_from_string_command: ICommandInfoV2 = {
-    exec: (state: EditorState, str: string) =>
-        change_actors_list(state, actor_list => [
-            ...actor_list,
-            ...load_map_from_string(str).actors
-        ])
+    exec: import_from_string
+}
+
+function import_from_string(state: EditorState, str: string): EditorState {
+    const imported_actors = load_map_from_string(str).actors
+    for (const actor of imported_actors){
+        actor.selected = true
+    }
+    const existing_actors = state.map.actors.map(a => {
+        if (a.selected){
+            const copy = a.shallow_copy()
+            copy.selected = false
+            return copy
+        }
+        else {
+            return a
+        }
+    })
+    return { ...state, map: { ... state.map, actors: [
+        ...existing_actors,
+        ...imported_actors
+    ]}}
 }
