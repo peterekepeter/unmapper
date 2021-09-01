e01e362f16edd29a6eff8c23ff0e7ed782f4493c
Eke Péter <peterekepeter@gmail.com>
2021-09-01 23:20:13 +0300

    Delete faces and edges.

--- src/commands/delete_selected.ts
+++ src/commands/delete_selected.ts
@@ -1,15 +1,15 @@
 import { ICommandInfoV2 } from "../controller/command"
-import { change_actors_list } from "../model/state/change_actors_list"
 import { deleteBrushData } from "../model/algorithms/deleteBrushData"
 import { EditorState } from "../model/EditorState"
 import { change_selected_brushes } from "../model/state"
-import { clear_selection } from "./selection/clear_selection"
+import { change_actors_list } from "../model/state/change_actors_list"
 import { pipe } from "../util/pipe"
+import { clear_selection } from "./selection/clear_selection"
 
 export const delete_selected_command: ICommandInfoV2 = {
     description: "Delete Selection",
     shortcut: "delete",
-    exec: pipe(delete_selected_data, clear_selection)
+    exec: pipe(delete_selected_data, clear_selection),
 }
 
 function delete_selected_data(state: EditorState): EditorState {
@@ -19,14 +19,12 @@ function delete_selected_data(state: EditorState): EditorState {
 }
 
 function delete_vertexes(state: EditorState) {
-    return change_selected_brushes(state, ((b, _, selection) => deleteBrushData(b, {
-        vertexes: selection.vertexes
-    })))
+    return change_selected_brushes(state, ((b, _, selection) => deleteBrushData(b, selection)))
 }
 
 function delete_actors(state: EditorState) {
     return change_actors_list(state, actors => {
-        const next_actors = actors.filter((_,index) => state.selection.actors.find(s => s.actor_index === index) == null)
+        const next_actors = actors.filter((_, index) => state.selection.actors.find(s => s.actor_index === index) == null)
         if (next_actors.length !== state.map.actors.length) {
             return next_actors
         }
