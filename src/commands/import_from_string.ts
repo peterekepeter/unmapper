c33355fa5f1ca325dacf6362f1ea1338f398ca71
Eke Péter <peterekepeter@gmail.com>
2021-09-20 23:29:20 +0300

    Fix select newly imported actors.

--- src/commands/import_from_string.ts
+++ src/commands/import_from_string.ts
@@ -2,22 +2,23 @@ import { ICommandInfoV2 } from "../controller/command"
 import { create_actor_selection } from "../model/EditorSelection"
 import { EditorState } from "../model/EditorState"
 import { load_map_from_string } from "../model/loader"
+import { range } from "../util/range"
 
-export const import_from_string_command: ICommandInfoV2 = {
-    exec: import_from_string
-}
+export const import_from_string_command: ICommandInfoV2 = { exec: import_from_string }
 
-function import_from_string(state: EditorState, str: string): EditorState {
-    const imported_actors = load_map_from_string(str).actors
-    return { ...state, 
+function import_from_string(state: EditorState, text: string): EditorState {
+    const imported_actors = load_map_from_string(text).actors
+    const first = state.map.actors.length
+    const last = first + imported_actors.length
+    return {
+        ...state, 
         map: { 
-                ... state.map, 
+            ... state.map, 
             actors: [
                 ...state.map.actors,
-                ...imported_actors
-            ]},
-        selection: { 
-            actors: imported_actors.map((a, i) => create_actor_selection(i)) 
-        }
+                ...imported_actors,
+            ], 
+        },
+        selection: { actors: range(first, last).map(create_actor_selection) },
     }
 }
