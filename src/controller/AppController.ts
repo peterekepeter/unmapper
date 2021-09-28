f15e3c9aa456b20758d56a8ef3631a8356ad5050
Eke Péter <peterekepeter@gmail.com>
2021-09-29 00:35:18 +0300

    BufferedInteractionController somewhat working.

--- src/controller/AppController.ts
+++ src/controller/AppController.ts
@@ -1,13 +1,14 @@
 import { createSignal, ISignal } from 'reactive-signals'
-import { create_history } from './history'
+
 import { create_initial_editor_state, EditorState } from '../model/EditorState'
+import { GeometryCache } from '../model/geometry/GeometryCache'
+import { UnrealMap } from '../model/UnrealMap'
+import { deep_freeze } from '../util/deep_freeze'
 import { create_command_registry } from './command/command_registry'
 import { ICommandInfoV2 } from "./command/ICommandInfoV2"
-import { UnrealMap } from '../model/UnrealMap'
-import { GeometryCache } from '../model/geometry/GeometryCache'
+import { create_history } from './history'
 import { InteractionController } from './interactions/InteractionController'
 import { InteractionRenderState } from './interactions/InteractionRenderState'
-import { deep_freeze } from '../util/deep_freeze'
 
 export class AppController {
 
@@ -23,7 +24,7 @@ export class AppController {
 
     history = create_history<UnrealMap>({
         get_state: () => this.current_state.map,
-        set_state: new_state => this.direct_state_change({ ...this.current_state, map: new_state })
+        set_state: new_state => this.direct_state_change({ ...this.current_state, map: new_state }),
     });
 
     constructor(initial_state?: EditorState) {
@@ -43,7 +44,7 @@ export class AppController {
 
         let current_state = this.current_state
         if (command_info.keep_status_by_default !== true && (current_state.status.is_error || current_state.status.message)){
-            current_state = { ... current_state, status: { is_error: false, message: '' }}
+            current_state = { ... current_state, status: { is_error: false, message: '' } }
         }
         
         next_state = command_info.exec(current_state, ...args)
@@ -66,14 +67,13 @@ export class AppController {
         if (next_state.interaction_render_state) {
             next_state = { ...next_state, interaction_render_state: null }
         }
-        next_state.interaction_render_state = null
         this.preview_state_change(next_state)
     }
 
     preview_command_with_interaction(
         interaction_render_state: InteractionRenderState,
         command_info: ICommandInfoV2,
-        args: unknown[]
+        args: unknown[],
     ): void {
         let next_state = command_info.exec(this.current_state, ...args)
         next_state = { ...next_state, interaction_render_state }
--- src/controller/interactions/BufferedInteractionController.ts
+++ src/controller/interactions/BufferedInteractionController.ts
@@ -1,9 +1,11 @@
 import { add_interaction_point_command } from "../../commands/interaction/add_interaction_point"
 import { replace_last_interaction_point_command } from "../../commands/interaction/replace_last_interaction_point"
+import { reset_interaction_buffer_command } from "../../commands/interaction/reset_interaction_buffer"
 import { Vector } from "../../model/Vector"
 import { ViewportEvent } from "../../model/ViewportEvent"
 import { AppController } from "../AppController"
 import { ICommandInfoV2 } from "../command"
+import { InteractionRenderState } from "./InteractionRenderState"
 
 export class BufferedInteractionController
 {
@@ -18,15 +20,29 @@ export class BufferedInteractionController
     }
 
     next_command(command_info: ICommandInfoV2): void {
+        if (this.command_info != null){
+            this.controller.execute(this.command_info)
+            this.controller.execute(reset_interaction_buffer_command)
+        }
         this.command_info = command_info
     }
 
-    handle_pointer_click(vector: Vector, event: ViewportEvent): void {
+    handle_pointer_click(vector: Vector, event: ViewportEvent, is_snap: boolean): void {
         this.controller.execute(add_interaction_point_command, vector, event.view_mode)
+        this.preview_current_command(vector, is_snap)
     }
 
-    handle_pointer_move(vector: Vector, event: ViewportEvent): void {
+    handle_pointer_move(vector: Vector, event: ViewportEvent, is_snap: boolean): void {
         this.controller.execute(replace_last_interaction_point_command, vector, event.view_mode)
+        this.preview_current_command(vector, is_snap)
+    }
+    private preview_current_command(vector: Vector, is_snap: boolean) {
+        const state = this.get_interaction_render_state(vector, is_snap)
+        this.controller.preview_command_with_interaction(state, this.command_info, [])
     }
 
+    private get_interaction_render_state(vector: Vector, is_snap: boolean): InteractionRenderState {
+        return { snap_location: is_snap ? vector : null }
+    }
+    
 }
