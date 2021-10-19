34d6692246a6e12df4981c0fcd3fb1a990eb8bea
Eke Péter <peterekepeter@gmail.com>
2021-10-19 23:56:47 +0300

    Separate scale for actor and geometry.

--- src/commands/scale.ts
+++ src/commands/scale.ts
@@ -1,6 +1,7 @@
 import { ICommandInfoV2 } from "../controller/command"
 import { FromTo, FromToInteraction } from "../controller/interactions/stateful/FromToInteraction"
 import { LocationInteraction } from "../controller/interactions/stateful/LocationInteraction"
+import { EditorState } from "../model/EditorState"
 import { get_world_to_actor_transform } from "../model/geometry/actor-space-transform"
 import { change_selected_actors, change_selected_brushes, change_selected_vertexes } from "../model/state"
 import { Vector } from "../model/Vector"
@@ -35,30 +36,42 @@ export const scale_command: ICommandInfoV2 = {
         const scaling_factor = scale.to.subtract_vector(pivot).length() 
             / scale.from.subtract_vector(pivot).length()
 
-        return state.options.vertex_mode ? change_selected_brushes(state, (b, a, s) => {
-
-            const transform_fn = get_world_to_actor_transform(a)
-            const actor_pivot = transform_fn(pivot)
-            return change_selected_vertexes(b, s.vertexes, v => v
-                .subtract_vector(actor_pivot)
-                .scale(scaling_factor)
-                .add_vector(actor_pivot))
-
-        }) : change_selected_actors(state, old => {
-
-            const new_location = old.location
-                .subtract_vector(pivot)
-                .scale(scaling_factor)
-                .add_vector(pivot)
-            const new_actor = old.shallow_copy()
-            new_actor.location = new_location
-            if (old.brushModel) {
-                const new_component_scaling = Vector.ONES.scale(scaling_factor)
-                new_actor.postScale = old.postScale.with_scale_vector(new_component_scaling)
-            } else {
-                // scale object
-            }
-            return new_actor
-        })
+        if (state.options.vertex_mode){
+            return scale_geometry_data(state, pivot, scaling_factor)
+        } else {
+            return scale_actors(state, pivot, scaling_factor)
+        }
     },
+}
+
+function scale_geometry_data(state: EditorState, pivot: Vector, scaling_factor: number): EditorState {
+    return change_selected_brushes(state, (b, a, s) => {
+
+        const transform_fn = get_world_to_actor_transform(a)
+        const actor_pivot = transform_fn(pivot)
+        return change_selected_vertexes(b, s.vertexes, v => v
+            .subtract_vector(actor_pivot)
+            .scale(scaling_factor)
+            .add_vector(actor_pivot))
+
+    })
+}
+
+function scale_actors(state: EditorState, pivot: Vector, scaling_factor: number): EditorState {
+    return change_selected_actors(state, old => {
+
+        const new_location = old.location
+            .subtract_vector(pivot)
+            .scale(scaling_factor)
+            .add_vector(pivot)
+        const new_actor = old.shallow_copy()
+        new_actor.location = new_location
+        if (old.brushModel) {
+            const new_component_scaling = Vector.ONES.scale(scaling_factor)
+            new_actor.postScale = old.postScale.with_scale_vector(new_component_scaling)
+        } else {
+            // scale object
+        }
+        return new_actor
+    })
 }
