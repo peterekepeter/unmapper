fdaf813d4fcf268b9c9f5221a6c882ced5bd2cfc
Eke Péter <peterekepeter@gmail.com>
2021-10-20 23:57:12 +0300

    Refactor scale command to use interaction buffer

--- src/commands/scale.ts
+++ src/commands/scale.ts
@@ -1,52 +1,51 @@
 import { ICommandInfoV2 } from "../controller/command"
-import { FromTo, FromToInteraction } from "../controller/interactions/stateful/FromToInteraction"
-import { LocationInteraction } from "../controller/interactions/stateful/LocationInteraction"
 import { EditorState } from "../model/EditorState"
 import { get_world_to_actor_transform } from "../model/geometry/actor-space-transform"
+import { DEFAULT_INTERACTION_BUFFER } from "../model/InteractionBuffer"
 import { change_selected_actors, change_selected_brushes, change_selected_vertexes } from "../model/state"
 import { Vector } from "../model/Vector"
 
 export const scale_command: ICommandInfoV2 = {
     description: 'Scale objects or vertexes',
     shortcut: 's',
-    args: [
-        {
-            name: "Pivot",
-            default_value: Vector.ZERO,
-            interaction_factory: LocationInteraction.factory,
-        },
-        {
-            name: "Scale from to",
-            interaction_factory: FromToInteraction.factory,
-            example_values: [
-                new FromTo(new Vector(1, 0, 0), new Vector(2, 0, 0)), // upscale
-                new FromTo(new Vector(2, 0, 0), new Vector(1, 0, 0)), // downscale
-                new FromTo(new Vector(1, 0, 0), new Vector(1, 0, 0)), // no change
-            ],
-        },
-    ],
-    exec: (state, pivot: Vector, scale: FromTo) => {
-        if (!scale || !scale.from || !scale.to || scale.from === scale.to){
-            return state
-        }
-        if (!pivot){
-            pivot = Vector.ZERO
-        }
+    uses_interaction_buffer: true,
+    exec: exec_scale_command,
+}
 
-        const scaling_factor = scale.to.subtract_vector(pivot).length() 
-            / scale.from.subtract_vector(pivot).length()
+function exec_scale_command(state: EditorState): EditorState {
+    const buffer= state.interaction_buffer
+    const points = buffer.points
+    if (points.length < 3){
+        return state
+    }
+    const [pivot, from, to] = points
+    const scaled = scale_selected(state, pivot, from, to)
+    return {
+        ...scaled,
+        interaction_buffer: DEFAULT_INTERACTION_BUFFER,
+    }
+}
 
-        if (state.options.vertex_mode){
-            return scale_geometry_data(state, pivot, scaling_factor)
-        } else {
-            return scale_actors(state, pivot, scaling_factor)
-        }
-    },
+function scale_selected(state: EditorState, pivot: Vector, from: Vector, to: Vector){
+    if (!from || !to || from === to){
+        return state
+    }
+    if (!pivot){
+        pivot = Vector.ZERO
+    }
+
+    const scaling_factor = to.subtract_vector(pivot).length() 
+        / from.subtract_vector(pivot).length()
+
+    if (state.options.vertex_mode){
+        return scale_geometry_data(state, pivot, scaling_factor)
+    } else {
+        return scale_actors(state, pivot, scaling_factor)
+    }
 }
 
 function scale_geometry_data(state: EditorState, pivot: Vector, scaling_factor: number): EditorState {
     return change_selected_brushes(state, (b, a, s) => {
-
         const transform_fn = get_world_to_actor_transform(a)
         const actor_pivot = transform_fn(pivot)
         return change_selected_vertexes(b, s.vertexes, v => v
