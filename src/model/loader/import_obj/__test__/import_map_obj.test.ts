1c30fea9d8d5a104af2422ebbf2d21d4a852ad3c
Eke Péter <peterekepeter@gmail.com>
2021-11-12 00:56:56 +0200

    WIP Obj import progress

--- src/model/loader/import_obj/__test__/import_map_obj.test.ts
+++ src/model/loader/import_obj/__test__/import_map_obj.test.ts
@@ -15,14 +15,14 @@ describe("importing test_data_obj_plane", () => {
         v 1.000000 0.000000 1.000000
         v -1.000000 0.000000 -1.000000
         v -1.000000 0.000000 1.000000
-        vt 0.625000 0.500000
-        vt 0.875000 0.500000
-        vt 0.875000 0.750000
-        vt 0.625000 0.750000
+        vt 0.000000 0.500000
+        vt 0.000000 0.000000
+        vt 0.500000 0.500000
+        vt 0.500000 0.000000
         vn 0.0000 1.0000 0.0000
         usemtl Material
         s off
-        f 1/1/1 3/2/1 4/3/1 2/4/1
+        f 1/1/1 3/3/1 4/4/1 2/2/1
     `
     
     let map: UnrealMap
@@ -36,17 +36,95 @@ describe("importing test_data_obj_plane", () => {
     test("1 actor imported", () => 
         expect(map.actors).toHaveLength(1))
 
+    test("actor is named Plane", () => 
+        expect(map.actors[0].name).toBe("Plane"))
+
     test("has 4 vertexes", () => 
         expect(brush.vertexes).toHaveLength(4))
 
     test("first vertex is 1, 0, -1", () => 
         expect(brush.vertexes[0].position).toEqual(new Vector(1, 0, -1)))
 
-    test("has 1 polygon", () => {
+    test("plane has 1 polygon", () => {
         expect(brush.polygons).toHaveLength(1)
     })
 
-    test("polygon has 4 vertexes", () => {
+    test("plane polygon has 4 vertexes", () => {
         expect(brush.polygons[0].vertexes).toHaveLength(4)
     })
+
+    test("plane polygon has texture string", () => {
+        expect(brush.polygons[0].texture).toBe("Material")
+    })
+
+    test("plane polygon has 4 edges", () => {
+        expect(brush.polygons[0].edges).toHaveLength(4)
+    })
+
+})
+
+describe("importing cube", () => {
+    /* eslint-disable spellcheck/spell-checker */
+    const wavefront_obj_data = `# Blender v2.92.0 OBJ File: ''
+        # www.blender.org
+        mtllib untitled.mtl
+        o Cube
+        v 1.000000 1.000000 -1.000000
+        v 1.000000 -1.000000 -1.000000
+        v 1.000000 1.000000 1.000000
+        v 1.000000 -1.000000 1.000000
+        v -1.000000 1.000000 -1.000000
+        v -1.000000 -1.000000 -1.000000
+        v -1.000000 1.000000 1.000000
+        v -1.000000 -1.000000 1.000000
+        vt 0.625000 0.500000
+        vt 0.875000 0.500000
+        vt 0.875000 0.750000
+        vt 0.625000 0.750000
+        vt 0.375000 0.750000
+        vt 0.625000 1.000000
+        vt 0.375000 1.000000
+        vt 0.375000 0.000000
+        vt 0.625000 0.000000
+        vt 0.625000 0.250000
+        vt 0.375000 0.250000
+        vt 0.125000 0.500000
+        vt 0.375000 0.500000
+        vt 0.125000 0.750000
+        vn 0.0000 1.0000 0.0000
+        vn 0.0000 0.0000 1.0000
+        vn -1.0000 0.0000 0.0000
+        vn 0.0000 -1.0000 0.0000
+        vn 1.0000 0.0000 0.0000
+        vn 0.0000 0.0000 -1.0000
+        usemtl Material
+        s off
+        f 1/1/1 5/2/1 7/3/1 3/4/1
+        f 4/5/2 3/4/2 7/6/2 8/7/2
+        f 8/8/3 7/9/3 5/10/3 6/11/3
+        f 6/12/4 2/13/4 4/5/4 8/14/4
+        f 2/13/5 1/1/5 3/4/5 4/5/5
+        f 6/11/6 5/10/6 1/1/6 2/13/6
+
+        `
+    let map: UnrealMap
+    let brush: BrushModel
+
+    beforeAll(() => {
+        map = fn(wavefront_obj_data)
+        brush = map?.actors[0]?.brushModel
+    })
+
+    test("1 actor imported", () => 
+        expect(map.actors).toHaveLength(1))
+
+    test("actor is named Cube", () => 
+        expect(map.actors[0].name).toBe("Cube"))
+
+    test("actor has brush model", () => 
+        expect(map.actors[0].brushModel).not.toBeNull())
+
+    test("has 4 vertexes", () => 
+        expect(brush.vertexes).toHaveLength(4))
+
 })
--- src/model/loader/import_obj/import_map_obj.ts
+++ src/model/loader/import_obj/import_map_obj.ts
@@ -1,9 +1,13 @@
 import { Actor } from "../../Actor"
+import { calculate_polygon_center } from "../../algorithms/calculate_polygon_median"
+import { calculate_polygon_normal } from "../../algorithms/calculate_polygon_normal"
 import { BrushModel } from "../../BrushModel"
 import { BrushPolygon } from "../../BrushPolygon"
 import { BrushVertex } from "../../BrushVertex"
 import { KnownClasses } from "../../KnownClasses"
 import { UnrealMap } from "../../UnrealMap"
+import { polygon_uv_from_vertex_uvs } from "../../uvmap/polygon_uv_from_vertex_uvs"
+import { set_brush_polygon_vertex_uvs } from "../../uvmap/vertex_uv"
 import { Vector } from "../../Vector"
 import { GenericParser } from "../common/GenericParser"
 import { tokenize_wavefront_obj, WavefrontCommandToken as T } from "./tokenize_wavefront_obj"
@@ -52,16 +56,28 @@ function parse_obj_actor(parser: GenericParser): Actor {
         actor.name = "Brush"
     }
     const brush = new BrushModel()
+    const uv_data: Vector[] = []
+    const normal_data: Vector[] = []
     actor.brushModel = brush
+    let texture: string|null = null
     
     let parsing_object = true
     while (parsing_object){
         switch (parser.get_current_token()){
+            case T.UseMaterial:
+                texture = parse_obj_texture(parser, texture)
+                break
+            case T.BeginVertexTexture:
+                uv_data.push(parse_obj_vertex_uv(parser))
+                break
+            case T.BeginVertexNormal:
+                normal_data.push(parse_obj_vertex_normal(parser))
+                break
             case T.BeginVertex:
-                brush.vertexes.push(parse_obj_vertex(parser))
+                brush.vertexes.push(parse_obj_vertex_position(parser))
                 break
             case T.BeginPolygon:
-                brush.polygons.push(parse_obj_polygon(parser, brush))
+                brush.polygons.push(parse_obj_polygon(parser, brush, texture, uv_data, normal_data))
                 break
             default: 
                 next_line(parser)
@@ -72,7 +88,15 @@ function parse_obj_actor(parser: GenericParser): Actor {
                 break
         }
     }
+    actor.brushModel.rebuild_all_poly_edges()
     return actor
+}
+
+function parse_obj_texture(parser: GenericParser, texture: string) {
+    parser.accept_and_move_to_next(T.UseMaterial)
+    texture = parser.get_current_token()
+    next_line(parser)
+    return texture
 }
 
 function next_line(parser: GenericParser) {
@@ -86,8 +110,28 @@ function next_line(parser: GenericParser) {
     }
 }
 
-function parse_obj_vertex(parser: GenericParser): BrushVertex {
+function parse_obj_vertex_normal(parser: GenericParser): Vector {
+    parser.accept_and_move_to_next(T.BeginVertexNormal)
+    const normal = parse_vector(parser)
+    next_line(parser)
+    return normal.normalize()
+}
+
+function parse_obj_vertex_uv(parser: GenericParser): Vector {
+    parser.accept_and_move_to_next(T.BeginVertexTexture)
+    const result = parse_vector(parser)
+    next_line(parser)
+    return result
+}
+
+function parse_obj_vertex_position(parser: GenericParser): BrushVertex {
     parser.accept_and_move_to_next(T.BeginVertex)
+    const result = parse_vector(parser)
+    next_line(parser)
+    return new BrushVertex(result)
+}
+
+function parse_vector(parser: GenericParser): Vector {
     let x = 0, y = 0, z = 0
     if (parser.get_current_token() != T.End)
     {
@@ -101,32 +145,68 @@ function parse_obj_vertex(parser: GenericParser): BrushVertex {
     {
         z = parser.parse_float_and_move_to_next()
     }
-    return new BrushVertex(new Vector(x, y, z))
+    return new Vector(x, y, z)
 }
 
-function parse_obj_polygon(parser: GenericParser, brush: BrushModel): BrushPolygon {
+function parse_obj_polygon(parser: GenericParser, brush: BrushModel, texture: string, uv_data: Vector[], normal_data: Vector[]): BrushPolygon {
     parser.accept_and_move_to_next(T.BeginPolygon)
     const vertex_index = []
     const texture_index = []
     const normal_index = []
     let token = parser.get_current_token()
     while (token !== T.End && token != null){
-        vertex_index.push(parser.parse_int_and_move_to_next())
+        vertex_index.push(parser.parse_int_and_move_to_next()-1)
         if (parser.get_current_token() === T.End){ break }
         if (parser.get_current_token() === '/'){
             parser.accept_and_move_to_next('/')
             if (parser.get_current_token() !== '/'){
-                texture_index.push(parser.parse_int_and_move_to_next())
+                texture_index.push(parser.parse_int_and_move_to_next()-1)
             }
             if (parser.get_current_token() === '/'){
                 parser.accept_and_move_to_next('/')
-                normal_index.push(parser.parse_int_and_move_to_next())
+                normal_index.push(parser.parse_int_and_move_to_next()-1)
             }
         }
         token = parser.get_current_token()
     }
     const polygon = new BrushPolygon()
     polygon.vertexes = vertex_index
+    polygon.texture = texture
+
+    const vertex_data = brush.vertexes.map(v => v.position)
+
+    // median
+    polygon.median = calculate_polygon_center(brush.vertexes, polygon)
+
+    // normal
+    if (normal_index.length === vertex_index.length) {
+        let x=0, y=0, z=0
+        for (const i of normal_index){
+            const vertex_normal = normal_data[normal_index[i]]
+            x += vertex_normal.x
+            y += vertex_normal.y
+            z += vertex_normal.z
+        }
+        polygon.normal = new Vector(x, y, z).normalize()
+    } else {
+        polygon.normal = calculate_polygon_normal(brush.vertexes, polygon)
+    }
+
+    // UV
+    if (texture_index.length === vertex_index.length){
+        const polygon_uv = polygon_uv_from_vertex_uvs(vertex_data, texture_index.map(i => uv_data[i]))
+        polygon.textureU = polygon_uv.textureU
+        polygon.textureV = polygon_uv.textureV
+        polygon.panU = polygon_uv.panU
+        polygon.panV = polygon_uv.panV
+        polygon.origin = polygon_uv.origin
+    } else {
+        polygon.origin = polygon.median
+        polygon.panU = 0
+        polygon.panV = 0
+        polygon.textureU = vertex_data[polygon.vertexes[1]].subtract_vector(vertex_data[polygon.vertexes[0]]).normalize()
+        polygon.textureV = polygon.textureU.cross(polygon.normal).normalize()
+    }
     return polygon
 }
 
