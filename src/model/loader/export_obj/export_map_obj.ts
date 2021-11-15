44ab80d28bba8877c58323b627d4ef042365f3b2
Eke Péter <peterekepeter@gmail.com>
2021-11-15 23:26:13 +0200

    fixed OBJ export for multiple objects

--- src/model/loader/export_obj/export_map_obj.ts
+++ src/model/loader/export_obj/export_map_obj.ts
@@ -5,50 +5,63 @@ import { get_actor_to_world_rotation_scaling, get_actor_to_world_transform } fro
 import { get_vertex_uv } from "../../uvmap/vertex_uv"
 import { Vector } from "../../Vector"
 
-export function export_map_obj(state: EditorState, only_selected: boolean): string {
-    const out_lines = ["# unmapper OBJ export v1"]
-    generate_wavefront_obj_from_state(state, only_selected, out_lines)
-    return out_lines.join("\n")
+type WavefrontExportState = {
+    vertex_count: number;
+    vertex_texture_count: number;
+    vertex_normal_count: number;
+    out_lines: string[];
 }
 
-function generate_wavefront_obj_from_state(state: EditorState, only_selected: boolean, out_lines: string[]) {
+export function export_map_obj(state: EditorState, only_selected: boolean): string {
+    const export_state: WavefrontExportState = {
+        vertex_count: 0,
+        vertex_normal_count: 0,
+        vertex_texture_count: 0,
+        // eslint-disable-next-line spellcheck/spell-checker
+        out_lines: ["# unmapper OBJ export v1"],
+    }
+    generate_wavefront_obj_from_state(state, only_selected, export_state)
+    return export_state.out_lines.join("\n")
+}
+
+function generate_wavefront_obj_from_state(state: EditorState, only_selected: boolean, export_state: WavefrontExportState) {
     let actors = state.map.actors
     if (only_selected){
         actors = state.selection.actors.map(s => actors[s.actor_index])
     }
     for (const actor of actors){
-        generate_wavefront_from_actor(actor, out_lines)
+        generate_wavefront_from_actor(actor, export_state)
     }
 }
 
-function generate_wavefront_from_actor(actor: Actor, out_lines: string[]) {
+function generate_wavefront_from_actor(actor: Actor, export_state: WavefrontExportState) {
     if (!actor.brushModel){
-        out_lines.push(`# not supported ${actor.className} ${actor.name}`)
+        export_state.out_lines.push(`# not supported ${actor.className} ${actor.name}`)
     } else {
-        out_lines.push(`o ${actor.name}`)
-        generate_wavefront_obj_from_brush(actor, actor.brushModel, out_lines)
+        export_state.out_lines.push(`o ${actor.name}`)
+        generate_wavefront_obj_from_brush(actor, actor.brushModel, export_state)
     }
 }
 
-function generate_wavefront_obj_from_brush(actor: Actor, brush: BrushModel, out_lines: string[]) {
+function generate_wavefront_obj_from_brush(actor: Actor, brush: BrushModel, export_state: WavefrontExportState) {
     const actor_to_world = get_actor_to_world_transform(actor)
     const actor_to_world_scale_rotate = get_actor_to_world_rotation_scaling(actor)
     // generate vertexes in world space
     for (const vertex of brush.vertexes){
         const world_position: Vector = actor_to_world(vertex.position)
-        out_lines.push(`v ${world_position.x} ${world_position.y} ${world_position.z}`)
+        export_state.out_lines.push(`v ${world_position.x} ${world_position.y} ${world_position.z}`)
     }
     // generate vertex texture coord for each vertex of each polygon
     for (const polygon of brush.polygons){
         for (const polygon_vertex_index of polygon.vertexes){
             const uv: Vector = get_vertex_uv(polygon, brush.vertexes[polygon_vertex_index].position)
-            out_lines.push(`vt ${uv.x} ${uv.y}`)
+            export_state.out_lines.push(`vt ${uv.x} ${uv.y}`)
         }
     }
     // generate 1 normal for each polygon
     for (const polygon of brush.polygons){
         const normal: Vector = actor_to_world_scale_rotate.apply(polygon.normal).normalize()
-        out_lines.push(`vn ${normal.x} ${normal.y} ${normal.z}`)
+        export_state.out_lines.push(`vn ${normal.x} ${normal.y} ${normal.z}`)
     }
     // polygon assembly
     let total_polygon_vertex_counter = 0
@@ -56,15 +69,18 @@ function generate_wavefront_obj_from_brush(actor: Actor, brush: BrushModel, out_
         const polygon_string_builder = ["f"]
         for (const polygon_vertex_index of brush.polygons[polygon_index].vertexes){
             // vertex list same as we use internally
-            const vertex_nr = polygon_vertex_index + 1 
+            const vertex_nr = polygon_vertex_index + 1 + export_state.vertex_count
             // generated 1 vertex texture for each vertex of each polygon
-            const texture_nr = total_polygon_vertex_counter + 1
+            const texture_nr = total_polygon_vertex_counter + 1 + export_state.vertex_texture_count
             // generate 1 normal for each polygon
-            const normal_nr = polygon_index + 1
+            const normal_nr = polygon_index + 1 + export_state.vertex_normal_count
             polygon_string_builder.push(`${vertex_nr}/${texture_nr}/${normal_nr}`)
             total_polygon_vertex_counter++
         }
-        out_lines.push(polygon_string_builder.join(" "))
+        export_state.out_lines.push(polygon_string_builder.join(" "))
     }
+    export_state.vertex_count += brush.vertexes.length
+    export_state.vertex_normal_count += brush.polygons.length
+    export_state.vertex_texture_count += total_polygon_vertex_counter
 }
 
