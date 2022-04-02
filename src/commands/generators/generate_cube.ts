f3355c713e529a803abbcd8613ad1a8f6c6880a0
Eke Péter <peterekepeter@gmail.com>
2022-04-02 22:24:32 +0300

    Generate cube better formatting

--- src/commands/generators/generate_cube.ts
+++ src/commands/generators/generate_cube.ts
@@ -1,3 +1,4 @@
+/* eslint-disable array-element-newline */
 import { ICommandInfoV2 } from "../../controller/command"
 import { Actor } from "../../model/Actor"
 import { BrushModel } from "../../model/BrushModel"
@@ -36,30 +37,12 @@ function generate_cube_model(name: string): BrushModel {
     }
 
     builder.add_quads([
-        0,
-        1,
-        3,
-        2,
-        2,
-        3,
-        7,
-        6,
-        6,
-        7,
-        5,
-        4,
-        4,
-        5,
-        1,
-        0,
-        3,
-        1,
-        5,
-        7,
-        0,
-        2,
-        6,
-        4,
+        0, 1, 3, 2,
+        2, 3, 7, 6,
+        6, 7, 5, 4,
+        4, 5, 1, 0,
+        3, 1, 5, 7,
+        0, 2, 6, 4,
     ].map(i => n + i))
 
     return builder.build()
